9f170f80d9f919e656bfc8fe2b3a0d0c19ab5681
MaSommer <martisom@stud.ntnu.no>
2017-09-21 15:59:17 +0200

    Reader in progres

--- Prosjektoppave/src/prosjektoppgave/Main.java
+++ Prosjektoppave/src/prosjektoppgave/Main.java
@@ -2,8 +2,8 @@ package prosjektoppgave;
 
 public class Main {
 	
-	public Main(){
-		
+	public static void main(String[] args) {
+		StockReader sr = new StockReader();
 	}
 
 }
--- Prosjektoppave/src/prosjektoppgave/StockReader.java
+++ Prosjektoppave/src/prosjektoppgave/StockReader.java
@@ -1,5 +1,7 @@
 package prosjektoppgave;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -14,8 +16,26 @@ public class StockReader {
 		readFile();
 	}
 	
-	public void readFile(){
-		Scanner sc = new Scanner(System.in);
+	public void readFile() {
+		Scanner scanner;
+		try {
+			scanner = new Scanner(new File("target/S&P500.csv"));
+			scanner.useDelimiter(",");
+			int c = 0;
+			while(scanner.hasNext()){
+				String nextCell = scanner.next();
+				
+				System.out.print(nextCell+" | ");
+				c++;
+				if (nextCell.equals("endrow") || c > 2){
+					System.exit(1);
+				}
+			}
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	
