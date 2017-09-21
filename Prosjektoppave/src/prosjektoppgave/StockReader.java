593913ee56be15eda1b3b9f9606eabf566c313dc
MaSommer <martisom@stud.ntnu.no>
2017-09-21 16:21:43 +0200

    Stocreader in progress

--- Prosjektoppave/src/prosjektoppgave/StockReader.java
+++ Prosjektoppave/src/prosjektoppgave/StockReader.java
@@ -2,32 +2,45 @@ package prosjektoppgave;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class StockReader {
 	
-	private Map<String, Double[][]> stockInformation;
+	private Map<Date, Double[]> stockInformation;
+	private String fromDate;
+	private String toDate;
+	private String file;
+	private int[] stocksSelected;
 	
 	
-	public StockReader(){
-		this.stockInformation = new HashMap<String, Double[][]>();
+	public StockReader(String file, String fromDate, String toDate, int[] stocksSelected){
+		this.file = file;
+		this.fromDate = fromDate;
+		this.toDate = toDate;
+		this.stocksSelected = stocksSelected;
+		this.stockInformation = new HashMap<Date, Double[]>();
 		readFile();
 	}
 	
 	public void readFile() {
 		Scanner scanner;
 		try {
-			scanner = new Scanner(new File("target/S&P500.csv"));
-			scanner.useDelimiter(",");
-			int c = 0;
+			scanner = new Scanner(new File("target/S&P500.txt"));
+			int row = 0;
 			while(scanner.hasNext()){
-				String nextCell = scanner.next();
-				
-				System.out.print(nextCell+" | ");
-				c++;
-				if (nextCell.equals("endrow") || c > 2){
+				String nextLine = scanner.nextLine();
+				String[] lineCells = nextLine.split("\t");
+				int col = 0;
+				for (String cell : lineCells) {
+					
+					col++;
+				}
+				System.out.println(nextLine+"");
+				row++;
+				if (row > 3){
 					System.exit(1);
 				}
 			}
